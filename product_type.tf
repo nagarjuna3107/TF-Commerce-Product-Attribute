cb40dc0ec37260c61765b76a8ac02077b63310f2
Nagarjun Yadav <nagarjunyadav.vangu@hcltech.com>
2024-10-22 16:21:57 +0530

    proucut file added

---
+++ product_type.tf
@@ -0,0 +1,96 @@
+resource "commercetools_product_type" "my-product-type2" {
+  key         = "my-product-type-key2"
+  name        = "Lens specification2"
+  description = "All the specific info concerning the lens"
+
+  attribute {
+    name = "autofocus"
+    label = {
+      en = "Has autofocus"
+      nl = "Heeft autofocus"
+    }
+    required = true
+    type {
+      name = "boolean"
+    }
+  }
+
+  attribute {
+    name = "lens_product_no"
+    label = {
+      en = "Lens product number"
+      nl = "Objectief productnummer"
+    }
+    required = true
+    type {
+      name = "text"
+    }
+    constraint = "Unique"
+    input_tip = {
+      en = "Enter the product code"
+      nl = "Voer de product code in"
+    }
+    searchable = true
+  }
+
+  attribute {
+    name = "previous_model"
+    label = {
+      en = "Previous model"
+      nl = "Vorig model"
+    }
+    type {
+      name              = "reference"
+      reference_type_id = "product"
+    }
+  }
+
+  attribute {
+    name = "some-flag"
+    label = {
+      en = "Some flag"
+      nl = "Een vlag"
+    }
+    required = false
+    type {
+      name = "enum"
+      value {
+        key   = "FLAG-1"
+        label = "Flag 1"
+      }
+      value {
+        key   = "FLAG-2"
+        label = "FLAG-2"
+      }
+    }
+  }
+
+  attribute {
+    name = "origin"
+    label = {
+      en = "Origin country"
+      nl = "Land van herkomst"
+    }
+    required = false
+    type {
+      name = "set"
+      element_type {
+        name = "lenum"
+        localized_value {
+          key = "NL"
+          label = {
+            en = "Netherlands"
+            nl = "Nederland"
+          }
+        }
+        localized_value {
+          key = "DE"
+          label = {
+            en = "Germany"
+            nl = "Duitsland"
+          }
+        }
+      }
+    }
+  }
+}
