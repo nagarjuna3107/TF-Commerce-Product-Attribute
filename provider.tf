5f53f7aa5ce14b219247e54a627f1949ea2d935f
Nagarjun Yadav <nagarjunyadav.vangu@hcltech.com>
2024-10-22 16:15:05 +0530

    terraform provider adding

---
+++ provider.tf
@@ -0,0 +1,12 @@
+terraform {
+  required_providers {
+    commercetools = {
+      source = "labd/commercetools"
+      version = "1.16.0"
+    }
+  }
+}
+
+provider "commercetools" {
+  # Configuration options
+}
