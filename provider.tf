30412eb9b6b3700c82abfad41b61ae5cea3a1dd9
Nagarjun Yadav <nagarjunyadav.vangu@hcltech.com>
2024-10-22 16:42:33 +0530

    commented the keys

--- provider.tf
+++ provider.tf
@@ -9,10 +9,10 @@ terraform {
 
 provider "commercetools" {
   # Configuration options
-  client_id     = "OJ0FbNgxAEnhEOW4VMTD33UM"
-  client_secret = "pT4hZceyb29ZA6a8CeCJMlqgMyKSVE9F"
-  project_key   = "naga-demo-project1"
+  # client_id     = "OJ0FbNgxAEnhEOW4VMTD33UM"
+  # client_secret = "pT4hZceyb29ZA6a8CeCJMlqgMyKSVE9F"
+  # project_key   = "naga-demo-project1"
   scopes        = "manage_project:naga-demo-project1 manage_api_clients:naga-demo-project1 view_api_clients:naga-demo-project1 view_audit_log:naga-demo-project1"
-  api_url       = "https://api.us-central1.gcp.commercetools.com"
+  # api_url       = "https://api.us-central1.gcp.commercetools.com"
   token_url     = "https://auth.us-central1.gcp.commercetools.com"  
 }
